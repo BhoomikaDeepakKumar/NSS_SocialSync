85884fccf02ec8f2b053fb4831795014029a4014
BhoomikaDeepakKumar <bhoomikakumar829@gmail.com>
2025-06-14 16:37:08 +0530

    logo

---
+++ springboot-website/src/test/java/net/javaproject/springboot_website/SpringbootWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaproject.springboot_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
